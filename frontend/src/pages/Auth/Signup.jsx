ed411ee960982ecec8e51aa08fac2178b87c193c
AkashParamanik <akashp1002@gmail.com>
2025-07-19 01:04:32 +0530

    frontend login signup backend port

--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
+import { validateEmail } from "../../utils/helper";
 
-const Signup = () => {
-  return <div>signup</div>;
+const Signup = ({ setCurrentPage }) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  //handle signup submit
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    let profileImageUrl = "";
+    if (!fullName) {
+      setError("Please enter full name");
+      return;
+    }
+    if (!validateEmail(email)) {
+      setError("Please enter valid email address.");
+      return;
+    }
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+    setError("");
+    //signup api call
+    try {
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again");
+      }
+    }
+  };
+  return (
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Create an account</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        {" "}
+        Join us today by entering your details below.
+      </p>
+      <form onSubmit={handleSignUp}>
+        <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-2">
+          <Input
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            label="Full Name"
+            placeholder="john"
+            type="text"
+          />
+
+          <Input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+          />
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password"
+            placeholder="min. 8 Characters"
+            type="password"
+          />
+        </div>
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+        <button type="submit" className="btn-primary">
+          SignUp
+        </button>
+        <p className="text-[13px] text-slate-800 mt-3 ">
+          Already Have an account?{" "}
+          <button
+            className="font-medium text-primary underline cursor-pointer "
+            onClick={() => {
+              setCurrentPage("login");
+            }}
+          >
+            LogIn
+          </button>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
